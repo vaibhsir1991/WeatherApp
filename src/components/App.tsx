7436c5c9a64d0aeedd17195c68ddde1d4e3af225
vaibhsir1991 <39051202+vaibhsir1991@users.noreply.github.com>
2021-03-14 12:15:18 +0530

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux';
 import { AppState } from '../redux/locationStateReducer';
 import GeoLocation from './_utils/GeoLocation/GeoLocation';
 
-const API_KEY = 'AIzaSyB_SEpGJP4yx0KrT2PUjAVdHJTN28bseJg';
-
 const App = (): React.ReactElement => {
   return (
     <Provider store={AppState}>
