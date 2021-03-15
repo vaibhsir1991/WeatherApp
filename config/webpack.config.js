0eb631e9428ba9ccc2fa39ab04429462a9eb8a2f
vaibhsir1991 <vaibhav_sirsikar@yahoo.com>
2021-03-15 14:27:43 +0530

    Fix Build Path

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
     filename: '[name].[contenthash].js'
   },
