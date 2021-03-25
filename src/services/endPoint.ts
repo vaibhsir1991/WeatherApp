253bdffe1648a3183dea9aa967d7355dd6fc71ed
vaibhsir1991 <vaibhav_sirsikar@yahoo.com>
2021-03-25 19:29:01 +0530

    Added HTTPS to endPoints

--- src/services/endPoint.ts
+++ src/services/endPoint.ts
@@ -2,7 +2,7 @@ export const apiEndPoints = {
   googleGeocodeAPI: (lat: number, long: number) =>
     `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.GOOGLE_API_KEY}`,
   openWeatherApiByCity: (city: string) =>
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_KEY}`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_KEY}`,
   openWeatherApiByCoordinates: (lat: number, long: number) =>
-    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.OPEN_WEATHER_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.OPEN_WEATHER_KEY}`
 };
